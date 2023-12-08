e4807ede42fdcb05f264137c9253292f8866f7f0
juancid08 <145454462+juancid08@users.noreply.github.com>
2023-12-08 22:42:32 +0100

    Add files via upload

--- Stranger things/js/index.js
+++ Stranger things/js/index.js
@@ -1,5 +1,4 @@
-// Página de JavaScript de mi página de black mirror
-// Juan Cid Benítez
+
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -12,13 +11,10 @@ function myFunction() {
 
 
   document.addEventListener('DOMContentLoaded', function () {
-    // Selecciona el botón y el contenedor de información
+    
     var button = document.getElementById('button');
     var informacion = document.getElementById('informacion');
-
-    // Agrega un evento de clic al botón
     button.addEventListener('click', function () {
-        // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
         if (informacion.style.display === 'none') {
             informacion.style.display = 'block';
         } else {
@@ -28,13 +24,9 @@ function myFunction() {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el botón y el contenedor de información
   var button2 = document.getElementById('button2');
   var informacion2 = document.getElementById('informacion2');
-
-  // Agrega un evento de clic al botón
   button2.addEventListener('click', function () {
-      // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
       if (informacion2.style.display === 'none') {
           informacion2.style.display = 'block';
       } else {
@@ -43,13 +35,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el botón y el contenedor de información
   var button3 = document.getElementById('button3');
   var informacion3 = document.getElementById('informacion3');
 
-  // Agrega un evento de clic al botón
   button3.addEventListener('click', function () {
-      // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
       if (informacion3.style.display === 'none') {
           informacion3.style.display = 'block';
       } else {
@@ -58,13 +47,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el botón y el contenedor de información
   var button4 = document.getElementById('button4');
   var informacion4 = document.getElementById('informacion4');
 
-  // Agrega un evento de clic al botón
   button4.addEventListener('click', function () {
-      // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
       if (informacion4.style.display === 'none') {
           informacion4.style.display = 'block';
       } else {
@@ -73,13 +59,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el botón y el contenedor de información
   var button5 = document.getElementById('button5');
   var informacion5 = document.getElementById('informacion5');
 
-  // Agrega un evento de clic al botón
   button5.addEventListener('click', function () {
-      // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
       if (informacion5.style.display === 'none') {
           informacion5.style.display = 'block';
       } else {
@@ -88,13 +71,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el botón y el contenedor de información
   var button6 = document.getElementById('button6');
   var informacion6 = document.getElementById('informacion6');
 
-  // Agrega un evento de clic al botón
   button6.addEventListener('click', function () {
-      // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
       if (informacion6.style.display === 'none') {
           informacion6.style.display = 'block';
       } else {
@@ -103,13 +83,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el botón y el contenedor de información
   var button7 = document.getElementById('button7');
   var informacion7 = document.getElementById('informacion7');
 
-  // Agrega un evento de clic al botón
   button7.addEventListener('click', function () {
-      // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
       if (informacion7.style.display === 'none') {
           informacion7.style.display = 'block';
       } else {
@@ -118,13 +95,10 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona el botón y el contenedor de información
   var button8 = document.getElementById('button8');
   var informacion8 = document.getElementById('informacion8');
 
-  // Agrega un evento de clic al botón
   button8.addEventListener('click', function () {
-      // Si el contenedor de información está oculto, muéstralo; de lo contrario, ocúltalo
       if (informacion8.style.display === 'none') {
           informacion8.style.display = 'block';
       } else {
